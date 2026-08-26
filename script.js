add455d7c3514166c9bd18ec25f36a56bf002407
ertoysrert-oss <ertoysr.ert@gmail.com>
2026-08-26 14:40:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("ARKİLER başlatıldı.");
+const postInput = document.querySelector(".create-content input");
+const postButton = document.querySelector(".post-button");
+
+postButton.addEventListener("click", function () {
+
+    const text = postInput.value.trim();
+
+    if (text === "") {
+        postInput.focus();
+        return;
+    }
+
+    alert("Gönderi sistemi Firebase bağlantısından sonra aktif olacak.");
+
+    postInput.value = "";
+});
